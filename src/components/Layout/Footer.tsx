3496fa264c220a2fb3a8a3d5938cc711b3b672f0
Tim Nguyen <timnguyen737@gmail.com>
2025-03-07 20:10:47 -0500

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,8 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-auto flex h-32 flex-col justify-center border-t border-t-secondary text-center">
       <div className="text-sm">
-        &copy; {new Date().getFullYear()} {process.env.AUTHOR_NAME}. All rights
-        reserved.
+        <a href="https://thisistim.dev">&copy; {new Date().getFullYear()} {process.env.AUTHOR_NAME}. All rights reserved.</a>
       </div>
     </footer>
   );
